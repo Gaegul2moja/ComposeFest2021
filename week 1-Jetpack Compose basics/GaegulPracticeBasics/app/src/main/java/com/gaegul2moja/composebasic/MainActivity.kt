2e4900e347afdeaaee8a3c5b5b92ca2d27f2a5b3
Gaegul2moja <gaegul2moja@gmail.com>
2021-11-07 02:23:32 +0900

    InitialCommit and JetPack Compose Basics till sub 12 lesson

---
+++ week 1-Jetpack Compose basics/GaegulPracticeBasics/app/src/main/java/com/gaegul2moja/composebasic/MainActivity.kt
@@ -0,0 +1,142 @@
+package com.gaegul2moja.composebasic
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.*
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.gaegul2moja.composebasic.ui.theme.ComposeBasicPracticeTheme
+
+/**
+ * import androidx.compose.ui.* //compose 사용 시, importing class 는 주로 아래 2개임을 참고하자
+ * import androidx.activity.compose.*
+ */
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeBasicPracticeTheme { //프로젝트 이름에 따라 네이밍이 달라짐
+                MyApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    val expanded = remember { mutableStateOf(false) }
+
+    val extraPadding by animateDpAsState(
+        targetValue = if (expanded.value) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        ) //조금 더 디테일한 애니메이션 (바운스 넣는 등)
+    )
+    //dp 를 받아 animation 을 보여주는 역할로, animationSpec 파라미터를 확장 가능
+
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+            ) {
+                Text(text = "Hello, ")
+                Text(text = name, style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                ))
+            }
+            OutlinedButton(
+                onClick = { expanded.value = !expanded.value }
+            ) {
+                Text(if (expanded.value) "Show less" else "Show more")
+            }
+        }
+    }
+    /**
+     * MaterialTheme.typography.h4.copy로 카피하여 일부 속성만 재지정해 사용한다.
+     * Modifier.fillMaxWith 는 match parent 의 역할을 수행
+     * Modifier.weight(1f) 는 사용가능한 공간을 모두 채운다
+     * Column 은 세로로 레이아웃을 구성한다. (Row, Box 등도 존재)
+     *
+     * surface 의 색을 변경하면 글자색 또한 자동으로 변하게 되는데, 이는,
+     * Surface 자체가 보편적으로 개발자가 사용할 색상을 기본으로 반영해 주기 때문이다.
+     * 즉, Surface가 primary이므로, 안에 들어간 객체는 onPrimary 가 자동으로 먹히게 된다.
+     *
+     * 대부분의 compose UI 요소들은 modifier 들을 파라미터로 받을 수 있도록 확장되어있다.
+     * Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+     * dp의 표현은 숫자 후 .을 넣어 단위 표기를 해야한다.
+     * */
+}
+
+@Preview(showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark")
+@Composable
+fun DefaultPreview() {
+    ComposeBasicPracticeTheme {
+        MyApp()
+    }
+}
+
+@Composable
+private fun Greetings(names: List<String> = List(1000) { "$it" } ) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
+            Greeting(name = name)
+        }
+    }
+    //코드의 중복 해결과 재사용을 위해 해당 Composable을 선언
+    //LazyColumn은 Recyclerview 와 유사하고, 큰 리스트를 사용 시 성능에 이점이 있다.
+    //RecyclerView 처럼 재활용하지는 않고 새로운 composable 들을 스크롤 시에 넣는다. 이는, 비용적으로 기존 방식보다 이득이라고 한다.
+}
+
+@Composable
+fun MyApp() {
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+    //화면 전환, 테마 변경, 권한 변경 시 activity 가 변환되며 기존에 내용이 날아가기 때문에 저장해 놓기 위해 사용
+
+    if (shouldShowOnboarding) {
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        //click 하면 button 이 false 가 된다 넘긴 clicked listener 는 내부의 button의 onclick으로 지정된다
+    } else {
+        Greetings()
+    }
+}
+
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinueClicked
+            ) {
+                Text("Continue")
+            }
+        }
+    }
+}
---
+++ week 1-Jetpack Compose basics/GaegulPracticeBasics/app/src/main/java/com/gaegul2moja/composebasic/ui/theme/Type.kt
@@ -0,0 +1,30 @@
+package com.gaegul2moja.composebasic.ui.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    body1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    )
+    // 새로운 TextStyle 을 커스텀할 수도 있지만, MaterialTheme.typography를 주로 사용하며
+    // 기본적으로 h1-h6, body1,body2, caption, subtitle1 등을 지원하고 있다.
+    /* Other default text styles to override
+    button = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp
+    ),
+    caption = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    )
+    */
+)
