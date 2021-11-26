fd02be527fdcb14cae4e5ce3f2089604a889942d
Gaegul2moja <gaegul2moja@gmail.com>
2021-11-27 02:05:42 +0900

    Jetpack Compose Using State finish

--- week 2-2-Using state in Jetpack Compose/start/src/main/java/com/codelabs/state/todo/TodoActivity.kt
+++ week 2-2-Using state in Jetpack Compose/start/src/main/java/com/codelabs/state/todo/TodoActivity.kt
@@ -21,6 +21,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import com.codelabs.state.ui.StateCodelabTheme
 
 class TodoActivity : AppCompatActivity() {
@@ -32,9 +35,27 @@ class TodoActivity : AppCompatActivity() {
         setContent {
             StateCodelabTheme {
                 Surface {
-                    // TODO: build the screen in compose
+                    TodoActivityScreen(todoViewModel)
                 }
             }
         }
     }
+
+    //이 composable 이 ViewModel (state 가 저장되어있는) 그리고 TodoScreen 의 composable의 브릿지가 되어줄 것
+    //TodoScreen composable 의 이벤트에 대한 처리로 viewmodel 을 호출
+    //todoViewModel.todoItems.observeAsState(listOf()) 는 todoViewModel에서 todoItems 라는 list 형태의 livedata 를 끌어온 것
+    //observeAsState 는 LiveData를 지켜보고, State 형태의 object로 변환하여 Compose 에서 값의 변화를 지켜볼 수 있도록 한다.
+    @Composable
+    private fun TodoActivityScreen(todoViewModel: TodoViewModel) {
+        TodoScreen(
+            items = todoViewModel.todoItems,
+            currentlyEditing = todoViewModel.currentEditItem,
+            onAddItem = todoViewModel::addItem,
+            onRemoveItem = todoViewModel::removeItem,
+            onStartEdit = todoViewModel::onEditItemSelected,
+            onEditItemChange = todoViewModel::onEditItemChange,
+            onEditDone = todoViewModel::onEditDone
+        )
+    }
+
 }
